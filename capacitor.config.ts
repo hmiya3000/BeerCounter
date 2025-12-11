17ef06b9993c2eae73c8fffecadf6f105a8686a7
Hironobu Miyazaki <hmiya2112@beetre.com>
2025-12-11 13:53:42 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BeerCounter',
+  webDir: 'www'
+};
+
+export default config;
